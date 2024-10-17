d6cd7524e7a15c400dcf88ffff1c6e06b20ae898
Eric <36512385+PhucNghi176@users.noreply.github.com>
2024-10-17 16:19:14 +0700

    Add SubjectCreated event and update version to 1.1.10
    Updated MBS-CONTRACT.SHARE.csproj to version 1.1.10.
    
    In DomainEvent.cs:
    - Added using directive for MBS_CONTRACT.SHARE.Abstractions.Messages.
    - Introduced namespace MBS_CONTRACT.SHARE.Services.Subjects.
    - Added static class DomainEvent with nested record SubjectCreated.
    - SubjectCreated implements IDomainEvent and ICommand interfaces.
    - SubjectCreated has properties: IdEvent (Guid), Name (string), Status (int), and SemesterId (Guid).

---
+++ MBS-CONTRACT.SHARE/Services/Subjects/DomainEvent.cs
@@ -0,0 +1,8 @@
+﻿using MBS_CONTRACT.SHARE.Abstractions.Messages;
+
+namespace MBS_CONTRACT.SHARE.Services.Subjects;
+
+public static class DomainEvent
+{
+    public record SubjectCreated(Guid IdEvent, string Name,int Status, Guid SemesterId) : IDomainEvent, ICommand;
+}

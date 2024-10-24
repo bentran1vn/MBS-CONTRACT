2bf0340bde4b5d3c6f0ad4417832424bc36335f7
Eric <36512385+PhucNghi176@users.noreply.github.com>
2024-10-24 22:32:37 +0700

    Update

--- MBS-CONTRACT.SHARE/Services/Slots/DomainEvent.cs
+++ MBS-CONTRACT.SHARE/Services/Slots/DomainEvent.cs
@@ -1,23 +1,23 @@
 ﻿using MBS_CONTRACT.SHARE.Abstractions.Messages;
 
 namespace MBS_CONTRACT.SHARE.Services.Slots;
-
 public static class DomainEvent
 {
-   //list of slot created
-   public record SlotsCreated(Guid IdEvent, IEnumerable<Slot> Slots) : IDomainEvent, ICommand;
+    //list of slot created
+    public record SlotsCreated(Guid IdEvent, IEnumerable<Slot> Slots) : IDomainEvent, ICommand;
 
-   public class Slot
-   {
-      public Guid? MentorId { get; set; }
-      public TimeOnly StartTime { get; set; }
-      public TimeOnly EndTime { get; set; }
-      public DateOnly Date { get; set; }
-      public bool IsOnline { get; set; }
-      public string? Note { get; set; }
-      public short? Month { get; set; }
-      public bool IsBook { get; set; }
-      public DateTimeOffset CreatedOnUtc { get; set; }
-      public DateTimeOffset? ModifiedOnUtc { get; set; }
+    public class Slot
+    {
+        public Guid SlotId { get; set; }
+        public Guid? MentorId { get; set; }
+        public TimeOnly StartTime { get; set; }
+        public TimeOnly EndTime { get; set; }
+        public DateOnly Date { get; set; }
+        public bool IsOnline { get; set; }
+        public string? Note { get; set; }
+        public short? Month { get; set; }
+        public bool IsBook { get; set; }
+        public DateTimeOffset CreatedOnUtc { get; set; }
+        public DateTimeOffset? ModifiedOnUtc { get; set; }
     }
 }
